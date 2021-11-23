73ebebd713f9fae98ffe7f6194e44c18f57cd386
angelesgiron <angelesfiguera11@gmail.com>
2021-11-23 15:03:04 -0500

    primera parte

---
+++ data.js
@@ -0,0 +1,27 @@
+let movies = [
+  {
+      name: 'Planeta',
+      des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+      image: 'descarga.jpg'
+  },
+  {
+      name: 'Planeta',
+      des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+      image: 'images (1).jpg'
+  },
+  {
+      name: 'Planeta',
+      des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+      image: 'images (2).jpg'
+  },
+  {
+      name: 'Planeta',
+      des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+      image: 'images (3).jpg'
+  },
+  {
+      name: 'Planeta',
+      des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+      image: 'images.jpg'
+  }
+]
